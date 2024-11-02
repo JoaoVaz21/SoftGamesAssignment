dde9ed3d89b77c3806ecead01ba11dff37adb954
JoaoVaz21 <87279576+JoaoVaz21@users.noreply.github.com>
2024-11-02 00:08:53 +0000

    Refactor magic words logic
    Change logic so we can easily add more configs

--- src/Projects/MagicWordsProject.ts
+++ src/Projects/MagicWordsProject.ts
@@ -2,18 +2,11 @@ import { Container, Sprite, Text, TextStyle, Texture } from 'pixi.js';
 import { IProject } from './IProject';
 import { BackButton } from './BackButton';
 
-export enum Configurations {
-    Text,
-    Image,
-    TextText,
-    TextImage,
-    ImageText,
-    TextImageText,
-}
+
 
 export class MagicWordsProject extends Container implements IProject {
     private texts: string[] = [
-        "test!",
+        "Yes, cats are great too",
         "Joao Vaz",
         "SOFTGAMES",
         "PIXI",
@@ -22,15 +15,29 @@ export class MagicWordsProject extends Container implements IProject {
     ];
 
     private imageUrls: string[] = [
-        '/assets/MagicWordsProject/Cat1.png', 
+        '/assets/MagicWordsProject/Cat1.png',
         '/assets/MagicWordsProject/Cat2.png',
         '/assets/MagicWordsProject/Cat3.png',
         '/assets/MagicWordsProject/Cat4.png',
     ];
+
+    private configurations: string[] = [
+        'T',
+        'I',
+        'TI',
+        'IT',
+        'TIT',
+        'ITI',
+        'IIT',
+        'TTI',
+        'IITI',
+        'ITII'
+    ]
     private intervalId: number = 0;
     private startX: number = 400;
-    private startY:number = 350;
-    private backFunction: ()=>void;
+    private startY: number = 350;
+    private offset: number = 20;
+    private backFunction: () => void;
     constructor(goToMainMenu: () => void) {
         super();
         this.backFunction = goToMainMenu;
@@ -59,167 +66,57 @@ export class MagicWordsProject extends Container implements IProject {
         // Clear previous content
         this.removeChildren();
 
-        const randomConfig = this.getRandomConfig();
-        console.log("random config: " + randomConfig);
-        switch (randomConfig) {
-            case Configurations.Image:
-                this.displayImageConfiguration();
-                break;
-            case Configurations.Text:
-                this.displayTextConfiguration();
-                break;
-            case Configurations.ImageText:
-                this.displayImageTextConfiguration();
-                break;
-            case Configurations.TextImage:
-                this.displayTextImageConfiguration();
-                break;
-            case Configurations.TextText:
-                this.displayTextTextConfiguration();
-                break;
-            case Configurations.TextImageText:
-                this.displayTextImageTextConfiguration();
-                break;
-        }
-    }
+        let currentX = this.startX;
+        let currentY = this.startY;
+        //Split config by letter
+        let randomConfig = this.getRandomConfig().split('');
 
-    private displayImageConfiguration() {
-        const randomImages = this.getRandomImages(1); 
-        const imageTexture = Texture.from(new URL(randomImages[0], import.meta.url).href); 
-        const imageSprite = new Sprite(imageTexture);
-        imageSprite.width = 50; 
-        imageSprite.height = 50; 
-        imageSprite.x = this.startX;
-        imageSprite.y = this.startY; 
-        this.addChild(imageSprite);
-    }
-
-    private displayTextConfiguration() {
-        const randomText = this.getRandomText();
-        const fontSize = this.getRandomFontSize();
-        const textStyle = new TextStyle({
-            fontSize: fontSize,
-            fill: '#FFFFFF', 
+        randomConfig.forEach(letter => {
+            if (letter == "I") {
+                let image = this.displayImageConfiguration(currentX, currentY);
+                currentX += image.width + this.offset;
+            }
+            if (letter == "T") {
+                let text = this.displayTextConfiguration(currentX, currentY);
+                currentX += text.width + this.offset;
+            }
         });
-        const textDisplay = new Text(randomText, textStyle);
-        textDisplay.x = this.startX; 
-        textDisplay.y = this.startY; 
-        this.addChild(textDisplay);
     }
-    private displayImageTextConfiguration(){
+
+    private displayImageConfiguration(x: number, y: number): Sprite {
         const randomImages = this.getRandomImages(1);
-        const imageTexture = Texture.from(new URL(randomImages[0], import.meta.url).href); 
+        const imageTexture = Texture.from(new URL(randomImages[0], import.meta.url).href);
         const imageSprite = new Sprite(imageTexture);
-        imageSprite.width = 50; 
-        imageSprite.height = 50; 
-        imageSprite.x = this.startX;
-        imageSprite.y = this.startY; 
+        imageSprite.width = 50;
+        imageSprite.height = 50;
+        imageSprite.x = x;
+        imageSprite.y = y;
         this.addChild(imageSprite);
+        return imageSprite
+    }
 
+    private displayTextConfiguration(x: number, y: number): Text {
         const randomText = this.getRandomText();
         const fontSize = this.getRandomFontSize();
         const textStyle = new TextStyle({
             fontSize: fontSize,
-            fill: '#FFFFFF', 
+            fill: '#FFFFFF',
         });
         const textDisplay = new Text(randomText, textStyle);
-        textDisplay.x = this.startX + imageSprite.height; 
-        textDisplay.y = this.startY; 
+        textDisplay.x = x;
+        textDisplay.y = y;
         this.addChild(textDisplay);
+        return textDisplay;
     }
-    private displayTextImageConfiguration(){
-        const randomText = this.getRandomText();
-        const fontSize = this.getRandomFontSize();
-        const textStyle = new TextStyle({
-            fontSize: fontSize,
-            fill: '#FFFFFF', 
-        });
-        const textDisplay = new Text(randomText, textStyle);
-        textDisplay.x = this.startX; 
-        textDisplay.y = this.startY; 
-        this.addChild(textDisplay);
-
-        const randomImages = this.getRandomImages(1); 
-        const imageTexture = Texture.from(new URL(randomImages[0], import.meta.url).href); 
-        const imageSprite = new Sprite(imageTexture);
-        imageSprite.width = 50; 
-        imageSprite.height = 50; 
-        imageSprite.x = textDisplay.x + textDisplay.width;
-        imageSprite.y = this.startY; 
-        this.addChild(imageSprite);
-
-    }
-
-    private displayTextTextConfiguration() {
-        const randomTextLeft = this.getRandomText();
-        const fontSizeLeft = this.getRandomFontSize();
-        const textStyleLeft = new TextStyle({
-            fontSize: fontSizeLeft,
-            fill: '#FFFFFF', 
-        });
-
-        const randomTextRight = this.getRandomText();
-        const fontSizeRight = this.getRandomFontSize();
-        const textStyleRight = new TextStyle({
-            fontSize: fontSizeRight,
-            fill: '#FFFFFF', 
-        });
-        const textDisplayRight = new Text(randomTextRight, textStyleRight);
-        const textDisplayLeft = new Text(randomTextLeft, textStyleLeft);
-        textDisplayLeft.x = this.startX;
-        textDisplayRight.x = this.startX + textDisplayLeft.width;
-        textDisplayLeft.y = this.startY;
-        textDisplayRight.y = this.startY;
-
-        this.addChild(textDisplayRight);
-        this.addChild(textDisplayLeft);
-
-    }
-    private displayTextImageTextConfiguration() {
-        const randomTextLeft = this.getRandomText();
-        const fontSizeLeft = this.getRandomFontSize();
-        const textStyleLeft = new TextStyle({
-            fontSize: fontSizeLeft,
-            fill: '#FFFFFF', 
-        });
-
-        const randomImages = this.getRandomImages(1); 
-        const imageTexture = Texture.from(new URL(randomImages[0], import.meta.url).href); 
-        const imageSprite = new Sprite(imageTexture);
-        imageSprite.width = 50; 
-        imageSprite.height = 50; 
-
-
-        const randomTextRight = this.getRandomText();
-        const fontSizeRight = this.getRandomFontSize();
-        const textStyleRight = new TextStyle({
-            fontSize: fontSizeRight,
-            fill: '#FFFFFF', 
-        });
-        const textDisplayRight = new Text(randomTextRight, textStyleRight);
-        const textDisplayLeft = new Text(randomTextLeft, textStyleLeft);
-        textDisplayLeft.x = this.startX;
-        imageSprite.x =this.startX + textDisplayLeft.width;
-        textDisplayRight.x = this.startX + textDisplayLeft.width + imageSprite.width;
-        textDisplayLeft.y = this.startY;
-        textDisplayRight.y = this.startY;
-        imageSprite.y = this.startY;
-
-        this.addChild(textDisplayRight);
-        this.addChild(imageSprite);
-        this.addChild(textDisplayLeft);
-
-    }
-
 
     private getRandomText(): string {
         const randomIndex = Math.floor(Math.random() * this.texts.length);
         return this.texts[randomIndex];
     }
-    private getRandomConfig(): Configurations {
-        const configs = Object.keys(Configurations);
-        const randomIndex = Math.floor(Math.random() * configs.length/2);
-        return randomIndex;
+
+    private getRandomConfig(): string {
+        const randomIndex = Math.floor(Math.random() * this.configurations.length);
+        return this.configurations[randomIndex];
     }
 
     private getRandomImages(count: number): string[] {
